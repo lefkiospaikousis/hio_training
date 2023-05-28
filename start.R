55a99d86c8935b64ae3ce4a0848797e29e391533
Lefkios Paikousis (WST) <lefkiospaik@gmail.com>
2023-05-29 02:25:01 +0300

    almost done

--- start.R
+++ start.R
@@ -1,16 +1,67 @@
 
 
-
-
 library(tidyverse)
 library(palmerpenguins)
+
+penguins
+
+
+glimpse(penguins)
+
+View(penguins)
+
+?penguins
+
+ggplot(data = penguins)
+
+# map visual properties (aesthetics) to axis
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+)
+
+
+# geom: the geometrical object that a plot uses to represent data
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point()
+
+
+
+# Adding aesthetics and layers
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
+) +
+  geom_point()
+
+# hen aesthetic mappings are defined in ggplot(), at the global level,
+# they’re passed down to each of the subsequent geom layers of the plot
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
+) +
+  geom_point() +
+  geom_smooth()
 
 
 ggplot(
   data = penguins,
   mapping = aes(x = flipper_length_mm, y = body_mass_g)
 ) +
-  geom_point(aes(color = sex)) +
+  geom_point(aes(color = species)) +
+  geom_smooth(method = "lm")
+
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(aes(color = species, shape = species)) +
   geom_smooth(method = "lm") +
   labs(
     title = "Body mass and flipper length",
@@ -18,3 +69,26 @@ ggplot(
     x = "Flipper length (mm)", y = "Body mass (g)",
     color = "Species", shape = "Species"
   )
+
+
+ggplot(penguins, aes(x = fct_infreq(species))) +
+  geom_bar(width = 0.5)
+
+
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_histogram(binwidth = 200)
+
+ggplot(penguins, aes(x = island, fill = species)) +
+  geom_bar()
+
+ggplot(penguins, aes(x = island, fill = species)) +
+  geom_bar(position = "fill")
+
+
+# Facets
+
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point(aes(color = species, shape = species)) +
+  facet_wrap(~island)
+
+
